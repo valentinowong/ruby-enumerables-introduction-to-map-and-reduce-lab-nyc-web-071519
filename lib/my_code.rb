48f66cb26e12ef22fed434d90123964063a10b62
Valentino Wong <valentino.wong@gmail.com>
2019-06-28 17:51:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,7 +39,7 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point=0)
-  num=0
+  num = 0
   value = starting_point
   source_array.length do
     value = value + source_array[num]
