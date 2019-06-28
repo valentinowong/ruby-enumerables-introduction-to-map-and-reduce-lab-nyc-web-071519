1d144270dd3ce12458018399055559c806e2ad18
Valentino Wong <valentino.wong@gmail.com>
2019-06-28 17:39:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,7 +40,7 @@ end
 
 def reduce_to_total(source_array, starting_point=0)
   value = 0
-  source_array.length.times do
+  (source_array.length - starting_point).times do
     value += source_array[starting_point]
     starting_point += 1
   end
