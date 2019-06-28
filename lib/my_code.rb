56b6965a8870a36a5a82e76b26f4e93273723a59
Valentino Wong <valentino.wong@gmail.com>
2019-06-28 17:24:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,7 +19,13 @@ def map_to_no_change(source_array)
 end
 
 def map_to_double(source_array)
-  
+  num = 0
+  array = []
+  source_array.length.times do
+    array << source_array[num]*2
+    num += 1
+  end
+  array
 end
 
 def map_to_square(source_array)
