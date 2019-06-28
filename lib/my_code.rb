b9f53917d7a14d0d17a216b825b3b2d1231d84d4
Valentino Wong <valentino.wong@gmail.com>
2019-06-28 17:09:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
-# My Code here....
+def map_to_negativize(source_array)
+  
+end
+
+def map_to_no_change(source_array)
+  
+end
+
+def map_to_double(source_array)
+  
+end
+
+def map_to_square(source_array)
+  
+end
+
+def re
