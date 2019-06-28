622e2736ee00288bc7f3d3efb57cbb5e89bb8b51
Valentino Wong <valentino.wong@gmail.com>
2019-06-28 17:32:30 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,7 +41,7 @@ end
 def reduce_to_total(source_array, starting_point=0)
   value = 0
   source_array.length.times do
-    value += source_array[starting_point]
+    value += source_array[starting_point+1]
     starting_point += 1
   end
   value
